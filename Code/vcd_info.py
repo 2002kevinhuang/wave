f5a68bf62a2d48bed8a5a78dffc246f71a1c2079
Kevin Huang <2002kevinhuang@gmail.com>
2020-07-22 08:56:23 -0400

    changed

--- Code/vcd_info.py
+++ Code/vcd_info.py
@@ -18,6 +18,10 @@ signal_abbreviation = []
 lineNumber = 0
 dictionaryNum = -1
 count = 0
+scope_index = -1
+signal_change = {}
+definition_end = 0
+temp_time = 0
 
 
 def header_info():
@@ -33,21 +37,24 @@ def header_info():
 
 
 def new_scope(scope_line):
-    scopeList.append(string[14:len(scope_line) - 6] + ": line " + str(lineNumber))
+    global scope_index
+    # scopeList.append(string[14:len(scope_line) - 6] + ": line " + str(lineNumber))
+    scope_index += 1
+    scopeList.append(string[14:len(scope_line) - 6] + " " + str(scope_index))
 
 
 for line in lines:
     lineNumber += 1
     string = line.rstrip()
     header_info()
-    if string[:6] == "$scope":
+    if "$scope" in string:
         new_scope(string)
         # type_abbreviation.append({})
         type_signal.append({})
         signal_abbreviation.append({})
         dictionaryNum += 1
         continue
-    if string[:4] == "$var":
+    if "$var" in string:
         if "wire" in string:
             count += 1
             type_signal[dictionaryNum]["wire " + str(count)] = string[14:-5]
@@ -55,19 +62,42 @@ for line in lines:
             # type_abbreviation[dictionaryNum][string[5:11]] = string[12]
         elif "reg" in string:
             count += 1
-            type_signal[dictionaryNum]["reg " + str(count)] = string[13:-5]
-            signal_abbreviation[dictionaryNum][string[13:-5]] = string[11]
+            type_signal[dictionaryNum]["reg " + str(count)] = string[12:-5]
+            signal_abbreviation[dictionaryNum][string[12:-5]] = string[11]
             # type_abbreviation[dictionaryNum][string[5:10]] = string[11]
+    if "$enddefinitions" in line:
+        definition_end = lineNumber
+
+for scope in signal_abbreviation:
+    for x in scope.values():
+        signal_change[x.lstrip()] = [[], []]
+# print(signal_change)
+
+
+for line in lines[definition_end:]:
+    string = line.lstrip().rstrip()
+    if string[0] == "#":
+        temp_time = string[1:]
+        continue
+    for scope in signal_abbreviation:
+        for abbreviation in scope.values():
+            if abbreviation != " ":
+                if (str(abbreviation) in string) and (string[0].isnumeric()) and (temp_time not in signal_change[abbreviation][0]):
+                    signal_change[abbreviation][0].append(temp_time)
+                    signal_change[abbreviation][1].append(string[0])
+                    # signal_change[abbreviation][temp_time] = string[0]
+'''
+print(signal_change)
 
 
 print("Date = " + date)
 print("Version = " + version)
 print("Timescale = " + version)
 print(scopeList)
-
 # for i in type_signal:
 #     print(i)
-
-for i in signal_abbreviation:
-    print(i)
-
+for i in type_signal:
+    print(i)'''
+#
+# for x in scopeList:
+#     print(x[-1])
--- Code/wave.py
+++ Code/wave.py
@@ -1,140 +1,254 @@
 import tkinter as tk
+from tkinter import ttk
+import ttkwidgets
 from tkinter import *
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter.filedialog import askopenfilename
+import vcd_info
 
-
+menuList = ["cut", "copy", "paste", "zoom fit", "zoom in", "zoom out", "undo", "previous", "next"]
 
 class waveformViewer():
     def __init__(self):
-        self.root= Tk()
+        self.root = Tk()
+        self.root.title('Waveform Viewer')
 
-        #menu
+        # fields
+        self.scope = ''
+        self.scopes = []
+        self.selected = []
+        self.emptyGraph=True
+
+        # menu
         self.menu()
 
-        #top row
+        # top row
         self.topframe = Frame(self.root)
-        self.topframe.pack()
+        self.topframe.pack(pady=10, anchor='w', padx=10)
         self.top()
 
-        #Bottom Row
-        self.botframe= Frame(self.root)
-        self.botframe.pack( side = BOTTOM )
-        
-        #left
-        self.leftframe = Frame(self.botframe)
-        self.leftframe.pack( side = LEFT )
+        # Bottom Row
+        self.botframe = Frame(self.root)
+        self.botframe.pack(side=BOTTOM)
+
+        # left
+        self.leftframe = Frame(self.botframe, padx=10, pady=20)
+        self.leftframe.pack(side=LEFT)
         self.left1 = Frame(self.leftframe)
         self.left1.pack()
+        self.space(self.leftframe)
         self.left2 = Frame(self.leftframe)
         self.left2.pack()
-        self.left3 = Frame(self.leftframe)
+        self.space(self.leftframe)
+        self.left3 = Frame(self.leftframe, highlightbackground="Grey", highlightthickness=1)
         self.left3.pack()
+        self.space(self.leftframe)
+        self.left4 = Frame(self.leftframe, highlightbackground="Grey", highlightthickness=1)
+        self.left4.pack()
         self.sst()
-        self.typeSig()
+        self.typeSig('start')
         self.Filter()
+        self.botbuttons()
 
-        #right
-        self.rightframe = Frame(self.botframe)
-        self.rightframe.pack( side = LEFT )
-        self.graph()
+        # mid
+        self.midframe = Frame(self.botframe, highlightbackground="Grey", highlightthickness=1)
+        self.midframe.pack(side=LEFT, anchor='n')
 
-        #start
+        # right
+        self.rightframe = Frame(self.botframe, padx=10)
+        self.rightframe.pack(side=LEFT, anchor='n')
+        self.graph('empty')
+        self.signals()
+
+        # start
         self.root.mainloop()
 
-    def top(self):
-        for i in range(4):
-            self.button1 = Button(self.topframe, text="cut")
-            self.button1.pack( side = LEFT)
+    def space(self, frame):
+        label = Label(frame, text=' ')
+        label.pack()
 
-            self.button2 = Button(self.topframe, text="paste")
+    def top(self):
+        global menuList
+        for x in menuList:
+            self.button2 = Button(self.topframe, text=x)
             self.button2.pack(side=LEFT)
-        
-        self.fromlabel=Label(self.topframe,text='From: ')
+
+        self.fromlabel = Label(self.topframe, text='From: ')
         self.fromlabel.pack(side=LEFT)
 
-        self.From = Text(self.topframe, height=1,width=10)
+        self.From = Text(self.topframe, height=1, width=10)
         self.From.pack(side=LEFT)
-        
-        self.tolabel=Label(self.topframe,text='To: ')
+
+        self.tolabel = Label(self.topframe, text='To: ')
         self.tolabel.pack(side=LEFT)
 
-        self.To = Text(self.topframe, height=1,width=10)
+        self.To = Text(self.topframe, height=1, width=10)
         self.To.pack(side=LEFT)
 
     def sst(self):
-        self.label=Label(self.left1,text='SST')
-        self.label.pack(side=TOP)
+        def selectItem(a):
+            curItem = self.tree.item(self.tree.focus())['text']
+            if curItem != self.scope:
+                self.scope = curItem
+                self.typeSig(self.scope)
+                self.signals.delete(0, END)
+                if self.emptyGraph==False:
+                    self.graph('empty')
 
-        self.listbox = Listbox(self.left1) 
-        self.listbox.pack(side = LEFT, fill = BOTH) 
-          
-        self.scrollbar = Scrollbar(self.left1) 
-        self.scrollbar.pack(side = RIGHT, fill = BOTH) 
-        for i in range(20): 
-            self.listbox.insert(END, "file "+str(i)) 
-        self.listbox.config(yscrollcommand = self.scrollbar.set) 
-        self.scrollbar.config(command = self.listbox.yview)
+        self.tree = ttk.Treeview(self.left1, height=6)
+        self.tree.column("#0", width=150, minwidth=150, stretch=NO)
+        self.tree.heading("#0", text="SST", anchor=W)
+        self.tf = self.tree.insert("", "end", text="map9v3_tb.vcd")  # put file name here
+        for x in vcd_info.scopeList:
+            self.scopes.append(x.split()[0])
+            self.tree.insert(self.tf, "end", text=x.split()[0])
+        self.tree.bind('<ButtonRelease-1>', selectItem)
+        treeScroll = ttk.Scrollbar(self.left1)
+        treeScroll.configure(command=self.tree.yview)
+        self.tree.configure(yscrollcommand=treeScroll.set)
+        treeScroll.pack(side=RIGHT, fill=Y)
+        self.tree.pack(side=LEFT)
 
-    def typeSig(self):
-        self.label1=Label(self.left2,text='Type | Signals')
-        self.label1.pack(side=TOP)
+    def typeSig(self, scope):  # you have to cntl click to selected multiple
+        def selectItem(a):
+            self.selected = [self.tree1.item(i) for i in self.tree1.selection()]
+            self.signals.delete(0, END)
+            for row in self.selected:
+                text = row['text'] + ' ' + ' '.join(row['values'])
+                self.signals.insert(END, text)
 
-        self.listbox1 = Listbox(self.left2) 
-        self.listbox1.pack(side = LEFT, fill = BOTH) 
-          
-        self.scrollbar1 = Scrollbar(self.left2) 
-        self.scrollbar1.pack(side = RIGHT, fill = BOTH) 
-        for i in range(20): 
-            self.listbox1.insert(END, "wire "+str(i)) 
-        self.listbox1.config(yscrollcommand = self.scrollbar1.set) 
-        self.scrollbar1.config(command = self.listbox1.yview)
+                for sc in vcd_info.signal_abbreviation:
+                    for signal in sc:
+                        if signal ==' '.join(text.split()[1:len(text.split())]):
+                            print(sc[signal])
+                            self.graph(sc[signal])
+
+        if scope == 'start':
+            self.tree1 = ttk.Treeview(self.left2, selectmode="extended")  # ttkwidgets.Checkbox
+            self.tree1["columns"] = (1)
+            self.tree1.column("#0", width=50, minwidth=50, stretch=NO)
+            self.tree1.heading("#0", text="Type", anchor=W)
+            self.tree1.column(1, width=100, minwidth=100, stretch=NO)
+            self.tree1.heading(1, text="Signals", anchor=W)
+            self.tree1.bind('<ButtonRelease-1>', selectItem)
+            treeScroll = ttk.Scrollbar(self.left2)
+            treeScroll.configure(command=self.tree1.yview)
+            self.tree1.configure(yscrollcommand=treeScroll.set)
+            self.tree1.pack(side=LEFT)
+            treeScroll.pack(side=RIGHT, fill=Y)
+        elif scope == 'map9v3_tb.vcd':
+            for row in self.tree1.get_children():
+                self.tree1.delete(row)
+            for s in vcd_info.type_signal:
+                for x in s:
+                    self.tree1.insert("", "end", text=x.rstrip()[:-2], values=(s[x].replace(' ', '\ ')))  # .lstrip()))
+        elif scope in self.scopes:
+            for row in self.tree1.get_children():
+                self.tree1.delete(row)
+            for scopeName in vcd_info.scopeList:
+                if scope == scopeName[:-2]:
+                    for x in vcd_info.type_signal[int(scopeName[-1])]:
+                        self.tree1.insert("", "end", text=x.rstrip()[:-2],
+                                          values=(vcd_info.type_signal[int(scopeName[-1])][x].replace(' ', '\ ')))
 
     def Filter(self):
-        self.flabel=Label(self.left3,text='Filter: ')
+        self.flabel = Label(self.left3, text='Filter: ')
         self.flabel.pack(side=LEFT)
 
-        self.filter = Text(self.left3, height=1,width=10)
+        self.filter = Text(self.left3, height=1, width=10)
         self.filter.pack(side=LEFT)
-    
-    def graph(self):
-        data1 = {'Country': ['US','CA','GER','UK','FR'],
-        'GDP_Per_Capita': [45000,42000,52000,49000,47000]}
-        df1 = DataFrame(data1,columns=['Country','GDP_Per_Capita'])
-        figure1 = plt.Figure(figsize=(6,5), dpi=100)
+
+    def botbuttons(self):
+        self.abutton = Button(self.left4, text="Append")
+        self.abutton.pack(side=LEFT)
+
+        self.ibutton = Button(self.left4, text="Insert")
+        self.ibutton.pack(side=LEFT)
+
+        self.rbutton = Button(self.left4, text="Replace")
+        self.rbutton.pack(side=LEFT)
+
+    def signals(self):
+        self.slabel = Label(self.midframe, text='Signals', width=10, anchor='w')
+        self.slabel.pack()
+
+        self.signals = Listbox(self.midframe, width=20, height=30)
+        self.signals.pack(fill=BOTH)
+
+    def graph(self,ab):
+        '''
+        input_amp = [1,0,1,0,1,0,1,0,1,0]
+        plt.style.use('dark_background')
+        plt.plot(input_amp, color="#39ff14",linewidth=.5,drawstyle='steps-post')
+        plt.title("Waveform")
+        plt.ylabel('Amplitude')
+        plt.yticks([0,1])
+        plt.xlabel("Time (Seconds)")
+        plt.xticks([i for i in range(10)])
+        plt.show()'''
+        if ab=='empty':
+            try:
+                self.canvas.get_tk_widget().destroy()
+            except:
+                pass
+            self.emptyGraph=True
+            figure = plt.Figure(figsize=(5, 4), dpi=120)
+            plot = figure.add_subplot(1, 1, 1, yticks=[0, 1], facecolor='black', title="Waveform", ylabel='Amplitude',xlabel="Time (Seconds)")
+            self.canvas = FigureCanvasTkAgg(figure, self.rightframe)
+            self.canvas.get_tk_widget().pack(side=RIGHT)
+            plot.plot([0,1],[0,1],color="#39ff14", linewidth=.5, drawstyle='steps-post')
+        else:
+            self.emptyGraph=False
+            self.canvas.get_tk_widget().destroy()
+            figure = plt.Figure(figsize=(5, 4), dpi=120)
+            plot = figure.add_subplot(1, 1, 1, yticks=[0, 1], facecolor='black', title="Waveform", ylabel='Amplitude',xlabel="Time (Seconds)")
+            self.canvas = FigureCanvasTkAgg(figure, self.rightframe)
+            self.canvas.get_tk_widget().pack(side=RIGHT)
+            data=vcd_info.signal_change[ab]
+            y = data[1]
+            x = data[0]
+            plot.plot(x,y,color="#39ff14", linewidth=.5, drawstyle='steps-post')       
+
+        '''
+        data1 = {'Country': ['US', 'CA', 'GER', 'UK', 'FR'],
+                 'GDP_Per_Capita': [45000, 42000, 52000, 49000, 47000]}
+        df1 = DataFrame(data1, columns=['Country', 'GDP_Per_Capita'])
+        figure1 = plt.Figure(figsize=(6, 5), dpi=100)
         ax1 = figure1.add_subplot(111)
         bar1 = FigureCanvasTkAgg(figure1, self.rightframe)
-        bar1.get_tk_widget().pack()
-        df1 = df1[['Country','GDP_Per_Capita']].groupby('Country').sum()
+        bar1.get_tk_widget().pack(side=RIGHT)
+        df1 = df1[['Country', 'GDP_Per_Capita']].groupby('Country').sum()
         df1.plot(kind='bar', legend=True, ax=ax1)
-        ax1.set_title('Country Vs. GDP Per Capita')
+        ax1.set_title('Country Vs. GDP Per Capita')'''
 
     def openFile(self):
         filename = askopenfilename()
         self.open.configure(text=filename)
 
+    '''
     def read(self):
         try:
-            #Tk().withdraw()
-            filename = 'map9v3_tb.vcd'#askopenfilename()   
-            file=open(filename,'r')
+            # Tk().withdraw()
+            filename = 'map9v3_tb.vcd'  # askopenfilename()
+            file = open(filename, 'r')
         except:
             print('file not found')
             return
 
-        wires={}
+        wires = {}
         for line in file.readlines():
             if '$var' in line:
-                params=line.split()
+                params = line.split()
                 print(params)
 
-        print(self.variables)
+        print(self.variables)'''
 
     def menu(self):
         def donothing(self):
             pass
+
         menubar = tk.Menu(self.root)
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label="New", command=donothing)
@@ -142,18 +256,19 @@ class waveformViewer():
         filemenu.add_command(label="Save", command=donothing)
         filemenu.add_separator()
         filemenu.add_command(label="Exit", command=self.root.quit)
-        menubar.add_cascade(label="File", menu=filemenu)      
+        menubar.add_cascade(label="File", menu=filemenu)
         editmenu = tk.Menu(menubar, tearoff=0)
         editmenu.add_command(label="Cut", command=donothing)
         editmenu.add_command(label="Copy", command=donothing)
-        editmenu.add_command(label="Paste", command=donothing)
+        editmenu.add_command(label="Past", command=donothing)
         editmenu.add_command(label="Duplicate Line", command=donothing)
         editmenu.add_command(label="Toggle Case", command=donothing)
-        menubar.add_cascade(label="Edit",menu=editmenu)
+        menubar.add_cascade(label="Edit", menu=editmenu)
         helpmenu = tk.Menu(menubar, tearoff=0)
         helpmenu.add_command(label="Help Index", command=donothing)
         helpmenu.add_command(label="About...", command=donothing)
         menubar.add_cascade(label="Help", menu=helpmenu)
         self.root.config(menu=menubar)
 
-wf=waveformViewer()
+
+wf = waveformViewer()
